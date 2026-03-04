0e1eb3cf39b88dce7ae8854d927f7309d6eba3d7
chedi bououn <chbouountito@gmail.com>
2026-03-04 13:29:14 +0200

    Feat:docker image build 4

--- src/file.js
+++ src/file.js
@@ -1,11 +1,15 @@
-const http = require('http');
+const express=require("express");
+const app=express()
+const path=require('path')
 require('dotenv').config();
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
 const port=process.env.PORT|| 8080;
-const server = http.createServer((req, res) => {
-  res.end('Hello from the Docker container!');
-});
 
-// This keeps the process "busy" listening for requests
-server.listen(port, () => {
+app.get('/',(req,res)=>{
+  res.render("home");
+})
+
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
